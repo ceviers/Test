71961346a03d441c459bd15f06a657c64b06ca8c
Cevier <Cevier@163.com>
2023-06-03 15:09:12 +0800

    线程池异常捕获

--- src/test/java/com/cevier/test/ThreadPoolTest2.java
+++ src/test/java/com/cevier/test/ThreadPoolTest2.java
@@ -3,6 +3,8 @@ package com.cevier.test;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
@@ -12,7 +14,19 @@ public class ThreadPoolTest2 {
     private static final ExecutorService executorService = new ThreadPoolExecutor(2, 3, 1, TimeUnit.MINUTES, new LinkedBlockingQueue<>()) {
         @Override
         protected void afterExecute(Runnable runnable, Throwable throwable) {
-            System.out.println(throwable.getMessage());
+            // execute提交
+            if (throwable != null)
+                System.out.println(throwable.getMessage());
+            //如果r的实际类型是FutureTask 那么是submit提交的，所以可以在里面get到异常
+            if (runnable instanceof FutureTask) {
+                try {
+                    Future<?> future = (Future<?>) runnable;
+                    //get获取异常
+                    future.get();
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+            }
         }
     };
 
@@ -20,7 +34,7 @@ public class ThreadPoolTest2 {
     public static void main(String[] args) throws InterruptedException {
         List<Integer> l = Arrays.asList(1, 2, 0, 3);
         for (Integer i : l) {
-            executorService.execute(() -> System.out.println(10 / i));
+            executorService.submit(() -> System.out.println(10 / i));
         }
         Thread.sleep(10_0000L);
     }
